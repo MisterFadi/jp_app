b2eaa07f63cd2c24d88b27c346bfe63d02e28366
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-10-16 01:59:48 +0200

    add new class GrafikenItem

---
+++ lib/models/grafiken_item_class.dart
@@ -0,0 +1,21 @@
+class GrafikenItem {
+  String imagePath;
+  String imageTitle;
+  String imageSubTitle;
+  String imagePrice;
+  String imageLikes;
+  String imageDescription;
+  String imageIngredients;
+  String imageReviews;
+
+  GrafikenItem({
+    required this.imagePath,
+    required this.imageTitle,
+    required this.imageSubTitle,
+    required this.imagePrice,
+    required this.imageLikes,
+    required this.imageDescription,
+    required this.imageIngredients,
+    required this.imageReviews,
+  });
+}
