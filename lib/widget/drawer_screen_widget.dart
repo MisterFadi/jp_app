d6c6619864293d9b36fcb67edd96eb528c02b51a
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-10-16 01:57:23 +0200

    add class DrawerScreenWidget

---
+++ lib/widget/drawer_screen_widget.dart
@@ -0,0 +1,39 @@
+/*
+class DrawerScreenWidget {
+  String imagePath;
+  String imageTitle;
+  String imageSubTitle;
+  String imagePrice;
+  String imageLikes;
+  String imageDescription;
+  String imageIngredients;
+  String imageReviews;
+  String title;
+  String imageDate;
+  String description;
+  String imagesubtitleDate;
+  String subtitle;
+  String price;
+  String likes;
+  String ingredients;
+  String reviews;
+
+  DrawerScreenWidget(
+      {required this.imagePath,
+      required this.imageTitle,
+      required this.imageSubTitle,
+      required this.imagePrice,
+      required this.imageLikes,
+      required this.imageDescription,
+      required this.imageIngredients,
+      required this.imageReviews,
+      required this.title,
+      required this.imageDate,
+      required this.description,
+      required this.subtitle,
+      required this.price,
+      required this.likes,
+      required this.ingredients,
+      required this.reviews});
+}
+*/
