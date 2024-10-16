fde4fe3eb796c4e4f3f1195dbcaeaa842d248ec4
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-10-16 02:00:20 +0200

    add new List<GrafikenItem>

---
+++ lib/repositories/grafiken_data.dart
@@ -0,0 +1,70 @@
+import 'package:jp_app/models/grafiken_item_class.dart';
+
+List<GrafikenItem> cakeData = [
+  GrafikenItem(
+    imagePath: "assets/grafiken/cat_cupcakes_3D.png",
+    imageTitle: "Mogli´s Cup",
+    imageSubTitle: "Strawberry ice cream",
+    imagePrice: "₳ 8.99",
+    imageLikes: "200",
+    imageDescription:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    imageIngredients: "Lecker",
+    imageReviews: "5 Sterne",
+  ),
+  GrafikenItem(
+    imagePath: "assets/grafiken/Ice.cream.png",
+    imageTitle: "Balu´s Cup",
+    imageSubTitle: "Strawberry ice cream",
+    imagePrice: "₳ 8.99",
+    imageLikes: "200",
+    imageDescription:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    imageIngredients: "Lecker",
+    imageReviews: "5 Sterne",
+  ),
+  GrafikenItem(
+    imagePath: "assets/grafiken/Burger_3D.png",
+    imageTitle: "Angi´s Yummy Burger",
+    imageSubTitle: "Delish vegan burger that tastes like heaven",
+    imagePrice: "₳ 13.99",
+    imageLikes: "30",
+    imageDescription:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    imageIngredients: "Lecker",
+    imageReviews: "4 Sterne",
+  ),
+  GrafikenItem(
+    imagePath: "assets/grafiken/ice_cream_stick_3D.png",
+    imageTitle: "Ali´s Choko Ice",
+    imageSubTitle: "Choko ice cream",
+    imagePrice: "₳ 3.99",
+    imageLikes: "200",
+    imageDescription:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    imageIngredients: "Lecker",
+    imageReviews: "5 Sterne",
+  ),
+  GrafikenItem(
+    imagePath: "assets/grafiken/chick_cupcakes_3D.png",
+    imageTitle: "Fadi´s Vanille Becher",
+    imageSubTitle: "Vanille Becher mit Schokosoße und Kirchen",
+    imagePrice: "₳ 4.99",
+    imageLikes: "200",
+    imageDescription:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    imageIngredients: "Lecker",
+    imageReviews: "5 Sterne",
+  ),
+  GrafikenItem(
+    imagePath: "assets/grafiken/Icecream_3D.png",
+    imageTitle: "Musti´s EisWaffel",
+    imageSubTitle: "Strawberry ice cream",
+    imagePrice: "₳ 8.99",
+    imageLikes: "200",
+    imageDescription:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    imageIngredients: "Lecker",
+    imageReviews: "5 Sterne",
+  ),
+];
